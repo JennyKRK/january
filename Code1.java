27eb80cf65eccc9d45001943521c95666fae56cf
JoannaN <joannan@student.agh.edu.pl>
2023-01-31 18:34:22 +0100

    Version new

--- Code1.java
+++ Code1.java
@@ -2,5 +2,6 @@ public class Code1{
 	public static void main(String[] args){
 		System.out.println("This is the first line of code");
 		System.out.println("*****************************");
+		System.out.println("HHHHHHHHHHHH");
 	}
 }

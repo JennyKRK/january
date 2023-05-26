552da7e3698ebb674d5d70df210e97e8bb6e769d
JoannaN <joannan@student.agh.edu.pl>
2023-05-26 13:42:25 +0200

    branchSimple

--- Code2.java
+++ Code2.java
@@ -1,4 +1,4 @@
-public class Code1{
+public class Code2{
 	public static void main(String[] args){
 		System.out.println("This is the first line of code");
 		System.out.println("*****************************");
